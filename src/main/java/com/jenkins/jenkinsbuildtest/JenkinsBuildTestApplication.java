6e65ffd49533861df9b4d179fa780865d14e4948
Rajasekar Nagarajan <rajasekar.jan1995@gmail.com>
2022-06-24 21:41:37 +0530

    initial commit

---
+++ src/main/java/com/jenkins/jenkinsbuildtest/JenkinsBuildTestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsbuildtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBuildTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/jenkinsbuildtest/JenkinsBuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsbuildtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuildTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
